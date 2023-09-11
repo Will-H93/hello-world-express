e8d8fe194b22689046cb772e2f122c0974a5bacc
Will Hodgkinson <73562324+Will-H93@users.noreply.github.com>
2023-09-11 22:25:58 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/hello', (_, res) => {
-  res.status(200).send("Hello March 2023!")
+  res.status(200).send("Hello May 2023!")
 })
 
 module.exports = app
